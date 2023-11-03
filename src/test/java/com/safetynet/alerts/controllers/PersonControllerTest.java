e41066cb5cffc30238c0aca06a0bc546ac20c0f2
Jilvo <jmillot41@gmail.com>
2023-11-03 15:09:54 +0100

    Impl : Tests

--- src/test/java/com/safetynet/alerts/controllers/PersonControllerTest.java
+++ src/test/java/com/safetynet/alerts/controllers/PersonControllerTest.java
@@ -1,8 +1,8 @@
 package com.safetynet.alerts.controllers;
+
 import com.safetynet.alerts.models.Person;
 import com.safetynet.alerts.services.JsonFileService;
-import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,23 +15,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(PersonController.class)
 public class PersonControllerTest {
-    @Mock
-    private MockMvc mockMvc;
-    @MockBean
-    public JsonFileService jsonFileService;
     @Autowired
-    private PersonController personController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JsonFileService jsonFileService;
+
 
     @Test
     public void testCreatePerson() throws Exception {
         Person person1 = new Person();
-        person1.setFirstName("John");
-        person1.setLastName("BoyAd");
-        person1.setAddress("1509 Culver St");
-        person1.setCity("Culver");
-        person1.setZip("97451");
-        person1.setPhone("841-874-6512");
-        person1.setEmail("jaboyd@email.com");
+        person1.setFirstName("Jhn");
+        person1.setLastName("BoAd");
+        person1.setAddress("1509Culver St");
+        person1.setCity("Culvr");
+        person1.setZip("9751");
+        person1.setPhone("841-74-6512");
+        person1.setEmail("jaoyd@email.com");
 
         mockMvc
                 .perform(post("/person").content(String.valueOf(person1)).contentType(MediaType.APPLICATION_JSON_VALUE))
